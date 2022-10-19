a94099d1fee76d69fe8e2e5dfd0167a7ab87bbbd
jacksonmonteiro <infor.jackson324@gmail.com>
2022-10-19 20:49:20 -0300

    AuthFormData model class, Some changes in AuthView, AuthForm Validation, AuthView and AuthForm validation function

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,27 @@
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({super.key});
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    super.key,
+    required this.onSubmit,
+  });
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formData = AuthFormData();
+  final _formKey = GlobalKey<FormState>();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,27 +30,66 @@ class AuthForm extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
+                if (_formData.isSignup)
+                  TextFormField(
+                    key: ValueKey('email'),
+                    initialValue: _formData.name,
+                    onChanged: (name) => _formData.name = name,
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (_name) {
+                      final name = _name ?? '';
+                      if (name.trim().length < 5) {
+                        return 'Name must be at least 5 characters';
+                      }
+
+                      return null;
+                    },
+                  ),
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                ),
-                TextFormField(
+                  key: ValueKey('name'),
+                  initialValue: _formData.email,
+                  onChanged: (email) => _formData.email = email,
                   decoration: InputDecoration(labelText: 'E-mail'),
+                  validator: (_email) {
+                    final email = _email ?? '';
+                    if (!email.contains('@')) {
+                      return 'Email is not a valid';
+                    }
+
+                    return null;
+                  },
                 ),
                 TextFormField(
+                  key: ValueKey('password'),
+                  initialValue: _formData.password,
+                  onChanged: (password) => _formData.password = password,
                   obscureText: true,
                   decoration: InputDecoration(labelText: 'Password'),
+                  validator: (_password) {
+                    final password = _password ?? '';
+                    if (password.length < 6) {
+                      return 'Password must be at least 6 characters';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 12),
                 ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Login'),
+                  onPressed: _submit,
+                  child: Text(_formData.isLogin ? 'Login' : 'Register'),
                 ),
                 TextButton(
-                  onPressed: () {},
-                  child: Text('Create a new account?')
-                ),
+                    onPressed: () {
+                      setState(() {
+                        _formData.toggleAuthMode();
+                      });
+                    },
+                    child: Text(_formData.isLogin
+                        ? 'Create a new account?'
+                        : 'Already have an account?')),
               ],
             ),
           ),
--- lib/views/auth_view.dart
+++ lib/views/auth_view.dart
@@ -1,16 +1,41 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthView extends StatelessWidget {
+class AuthView extends StatefulWidget {
   const AuthView({super.key});
+
+  @override
+  State<AuthView> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData data) {
+    setState(() => _isLoading = true);
+    setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(
+                onSubmit: _handleSubmit,
+              ),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+              child: Center(child: CircularProgressIndicator()),
+            ),
+        ],
       ),
     );
   }
