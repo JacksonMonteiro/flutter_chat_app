319920425f6e0b4a3aef4750ec3714e3a9114b3a
jacksonmonteiro <infor.jackson324@gmail.com>
2022-10-21 14:47:16 -0300

    Foreground push notifications - firebase

--- lib/services/notification/chat_notification_service.dart
+++ lib/services/notification/chat_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -14,5 +15,31 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  // Push Notification
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_messageHandle);
+    }
+  }
+
+  void _messageHandle(RemoteMessage? message) {
+    if (message == null || message.notification == null) return;
+
+    add(ChatNotification(
+      title: message.notification?.title ?? 'Não Informado',
+      body: message.notification?.body ?? 'Não informado',
+    ));
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
   }
 }
--- lib/views/auth_or_app_view.dart
+++ lib/views/auth_or_app_view.dart
@@ -2,19 +2,21 @@ import 'package:chat/firebase_options.dart';
 import 'package:chat/models/chat_user.dart';
 import 'package:chat/services/auth/auth_service.dart';
 import 'package:chat/services/auth/auth_service_mock.dart';
+import 'package:chat/services/notification/chat_notification_service.dart';
 import 'package:chat/views/auth_view.dart';
 import 'package:chat/views/chat_view.dart';
 import 'package:chat/views/loading_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppView extends StatelessWidget {
   const AuthOrAppView({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+        options: DefaultFirebaseOptions.currentPlatform);
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
