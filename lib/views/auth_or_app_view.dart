9b361b30c272255d5227a03b1590ab506416e88b
jacksonmonteiro <infor.jackson324@gmail.com>
2022-10-21 10:35:59 -0300

    Firebase Added to Project

--- lib/views/auth_or_app_view.dart
+++ lib/views/auth_or_app_view.dart
@@ -5,22 +5,34 @@ import 'package:chat/views/auth_view.dart';
 import 'package:chat/views/chat_view.dart';
 import 'package:chat/views/loading_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppView extends StatelessWidget {
   const AuthOrAppView({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-          stream: AuthService().userChanges,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return LoadingView();
-            } else {
-              return snapshot.hasData ? ChatView() : AuthView();
-            }
-          }),
+    return FutureBuilder(
+      future: init(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LoadingView();
+        } else {
+          return StreamBuilder<ChatUser?>(
+              stream: AuthService().userChanges,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return LoadingView();
+                } else {
+                  return snapshot.hasData ? ChatView() : AuthView();
+                }
+              });
+        }
+      },
     );
   }
 }
